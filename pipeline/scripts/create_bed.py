8b1fd1994bad370936e94af56befa8abe6e36d1d
andynet <andrejbalaz001@gmail.com>
2019-10-08 14:18:21 +0200

    added input function and next step

---
+++ pipeline/scripts/create_bed.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 11:21:18 2019
+
+@author: andyb
+"""
+
+# %%
+from Bio import SeqIO
+import argparse
+import gzip
+
+# %%
+parser = argparse.ArgumentParser(description="Creating bed file")
+parser.add_argument('--input', required=True)
+parser.add_argument('--bin_size', required=True)
+parser.add_argument('--output', required=True)
+args = parser.parse_args()
+
+# %%
+with gzip.open(args.input, "rt") as in_fd, open(args.output, 'w') as out_fd:
+    chromosomes = [f"chr{i}" for i in range(1, 23)]
+    records = [record for record in SeqIO.parse(in_fd, 'fasta') if record.id in chromosomes]
+    records = sorted(records, key=lambda x: int(x.id[3:]))
+
+    for record in records:
+        for i in range(0, len(record.seq), args.bin_size):
+            Ns = record.seq[i:i + 1000].count('N')
+            out_fd.write(f"{record.id}\t{i}\t{min(i + 1000, len(record.seq))}\t{Ns}\n")
+
+# %%
+# [f(x) if condition else g(x) for x in sequence]
+# [f(x) for x in sequence if condition]
---
+++ pipeline/scripts/input_functions.py
@@ -0,0 +1,9 @@
+def get_file_ids(wildcards):
+    import pandas as pd
+    from string import Template
+    manifest = f"{wildcards.data_dir}/manifests/{wildcards.ctype}.filtered.tsv"
+    manifest_df = pd.read_csv(manifest, sep='\t')
+    file_ids = list(manifest_df['file_id'])
+    out = Template(f"{wildcards.data_dir}/bams/remapped/{wildcards.ctype}/$file_id.bam")
+    return [out.substitute(file_id=file_id) for file_id in file_ids]
+
