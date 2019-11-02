6b9a763c1da0b75ceded1789429135d0ef52a483
andynet <andrejbalaz001@gmail.com>
2019-11-02 16:48:03 +0100

    added onerror message

--- pipeline/scripts/input_functions.py
+++ pipeline/scripts/input_functions.py
@@ -7,3 +7,11 @@ def get_file_ids(wildcards):
     out = Template(f"{wildcards.data_dir}/bams/remapped/{wildcards.ctype}/$file_id.bam")
     return [out.substitute(file_id=file_id) for file_id in file_ids]
 
+def get_download_ids(wildcards):
+    import pandas as pd
+    from string import Template
+    manifest = f"{wildcards.data_dir}/manifests/{wildcards.ctype}.filtered.tsv"
+    manifest_df = pd.read_csv(manifest, sep='\t')
+    file_ids = list(manifest_df['file_id'])
+    out = Template(f"{wildcards.data_dir}/bams/original/{wildcards.ctype}/$file_id.bam")
+    return [out.substitute(file_id=file_id) for file_id in file_ids]
