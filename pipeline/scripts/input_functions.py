03cd6c836e2947dbf4a43c3433a36814f97ea8e1
andynet <andrejbalaz001@gmail.com>
2019-11-19 16:44:10 +0100

    added hmmcopy parts; autodownload of reference and mapability; added download-independent remap rule;

--- pipeline/scripts/input_functions.py
+++ pipeline/scripts/input_functions.py
@@ -1,17 +1,28 @@
+import pandas as pd
+from string import Template
+from glob import glob
+
+
+# %%
 def get_file_ids(wildcards):
-    import pandas as pd
-    from string import Template
     manifest = f"{wildcards.data_dir}/manifests/{wildcards.ctype}.filtered.tsv"
     manifest_df = pd.read_csv(manifest, sep='\t')
     file_ids = list(manifest_df['file_id'])
     out = Template(f"{wildcards.data_dir}/bams/remapped/{wildcards.ctype}/$file_id.bam")
     return [out.substitute(file_id=file_id) for file_id in file_ids]
 
+
+# %%
 def get_download_ids(wildcards):
-    import pandas as pd
-    from string import Template
     manifest = f"{wildcards.data_dir}/manifests/{wildcards.ctype}.filtered.tsv"
     manifest_df = pd.read_csv(manifest, sep='\t')
     file_ids = list(manifest_df['file_id'])
     out = Template(f"{wildcards.data_dir}/bams/original/{wildcards.ctype}/$file_id.bam")
     return [out.substitute(file_id=file_id) for file_id in file_ids]
+
+
+# %%
+def get_downloaded_to_remapped(wildcards):
+    path = f"{wildcards.data_dir}/bams/original/{wildcards.ctype}/*.bam"
+    original = glob(path)
+    return [x.replace("original", "remapped") for x in original]
